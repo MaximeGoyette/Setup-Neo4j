c1ef9e68514e2b2d7c992580a0f82d9cee4ec8ec
Maxime Goyette <maxime.goyette.2@etsmtl.net>
2020-06-20 17:08:56 -0400

    Added support for custom username and initialization cypher script.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM docker:stable
 
+COPY apoc.conf /apoc.conf
 COPY entrypoint.sh /entrypoint.sh
+
 RUN chmod +x /entrypoint.sh
+
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker_run="docker run"
-docker_run="$docker_run -d -p 7474:7474 --env=NEO4J_AUTH=neo4j/$INPUT_PASSWORD neo4j:$INPUT_TAG"
-
-sh -c "$docker_run"
+echo "input username: $INPUT_USERNAME"
+echo "input tag: $INPUT_TAG"
+echo $(pwd)/$INPUT_INITIALIZERPATH
+docker run --name neo4j -d -p 7687:7687 -p 7474:7474 -e NEO4J_AUTH=$INPUT_USERNAME/$INPUT_PASSWORD -e NEO4JLABS_PLUGINS="[\"apoc\"]" -v $(pwd)/$INPUT_INITIALIZERPATH:/var/lib/neo4j/import/initializer.cypher:ro -v /apoc.conf:/var/lib/neo4j/conf/apoc.conf:ro neo4j:$INPUT_TAG
